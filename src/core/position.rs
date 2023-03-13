ae3738c57ccf74364a3bb9ab14a7ea80d97cc7fa
Skyrbunny <tallesttower1@gmail.com>
2023-03-13 00:43:36 -0700

    got inheritance to work, i guess

--- src/core/position.rs
+++ src/core/position.rs
@@ -1,8 +1,11 @@
 use std::cell::RefCell;
 
 use magnus::{
-    class, define_class, function, init, method, module, Attr, Class, Error, Module, Object,
+    class, define_class, function, init, method, module, prelude::*, rb_sys::AsRawValue,
+    typed_data, wrap, Attr, Class, Error, Module, Object, TypedData,
 };
+
+use rb_sys::{rb_define_alloc_func, rb_obj_reveal};
 
 #[derive(Clone, Copy)]
 pub struct Position {
@@ -15,8 +18,37 @@ pub struct Position {
 pub struct MutPosition(pub std::cell::RefCell<Position>);
 
 impl MutPosition {
-    pub fn new(x: f64, y: f64, z: f64) -> Self {
-        Self(RefCell::new(Position { x, y, z }))
+    // Allocator function to produce a new 'uninitialised' MutPoint.
+    //
+    // Argument is a Ruby VALUE (pointer to an object) for the class that is
+    // being allocated.
+    //
+    // Return value is a Ruby VALUE (pointer to an object) of the uninitialised
+    // object.
+    extern "C" fn alloc(class: u64) -> u64 {
+        // create the Rust struct. The idea of uninitialised data doesn't
+        // really make sense in Rust, so we'll just use default values.
+        let point = Self(RefCell::new(Position {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }));
+        // wrap the struct as a Ruby object then get the raw Ruby VALUE pointer
+        let instance = typed_data::Obj::wrap(point).as_raw();
+        // Magnus' wrap function (which is the only way of wrapping data with
+        // and object in Magnus < 0.6) always uses the default class, we need
+        // to update that to the passed class, incase it has been subclassed in
+        // Ruby.
+        unsafe { rb_obj_reveal(instance, class) };
+        // Return the instance
+        instance
+    }
+
+    fn initialize(&self, x: f64, y: f64, z: f64) {
+        let mut this = self.0.borrow_mut();
+        this.x = x;
+        this.y = y;
+        this.z = z
     }
 
     pub fn distance(&self, other: &MutPosition) -> f64 {
@@ -61,14 +93,31 @@ pub trait HasPosition {
 
 pub fn init() -> Result<(), Error> {
     let class = define_class("Position", class::object())?;
-    class.define_singleton_method("new", function!(MutPosition::new, 3))?;
+    // Let Ruby know about our allocator function
+    unsafe {
+        rb_define_alloc_func(
+            // call `class()` from the `TypedData` trait *before* defining
+            // our allocator, because in Magnus < 0.6 the default
+            // implementation of `TypedData::class()` undefs the allocator
+            // function on first call. So we want to get that out of the way
+            // before defining the allocator function.
+            // It just so happens that `rb_define_alloc_func` wants the same
+            // class as an arg, so we can kill two birds with one stone.
+            <MutPosition as TypedData>::class().as_raw(),
+            Some(MutPosition::alloc),
+        )
+    };
+
+    class
+        .define_method("initialize", method!(MutPosition::initialize, 3))
+        .unwrap();
     class.define_method("distance", method!(MutPosition::distance, 1))?;
     class.define_method("distancesq", method!(MutPosition::distancesq, 1))?;
     class.define_method("x", method!(MutPosition::x, 0))?;
-    class.define_method("set_x", method!(MutPosition::set_x, 1))?;
+    class.define_method("x=", method!(MutPosition::set_x, 1))?;
     class.define_method("y", method!(MutPosition::y, 0))?;
-    class.define_method("set_y", method!(MutPosition::set_y, 1))?;
+    class.define_method("y=", method!(MutPosition::set_y, 1))?;
     class.define_method("z", method!(MutPosition::z, 0))?;
-    class.define_method("set_z", method!(MutPosition::set_z, 1))?;
+    class.define_method("z=", method!(MutPosition::set_z, 1))?;
     Ok(())
 }
--- src/ruby/test.rb
+++ src/ruby/test.rb
@@ -5,18 +5,29 @@ class Test
   end
 end
 
+class Test2 < Test
+  def foo
+    puts "bar"
+  end
+end
+
 class AlsoPosition < Position
+  def initialize(x,y,z)
+    puts "init!"
+    super
+  end
   def foo()
     puts "bar"
   end
 end
 
 Test.new.run
+Test2.new.foo
 pos = Position.new(1, 2, 3)
 puts pos.x
-pos.set_x(15)
+pos.x = 15
 puts pos.x
 a_pos = AlsoPosition.new(1,2,3)
 puts a_pos.y
-#puts a_pos.foo
-
+puts a_pos.class
+puts a_pos.foo
